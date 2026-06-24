d2d2f4454999968b70f845585c2e961571a61c3e
Ali Al-Shanti <alialshanti998@gmail.com>
2026-06-24 07:20:27 +0300

    feat(dashboard): add revenue chart widget

--- modules/dashboard/components/revenue-chart.tsx
+++ modules/dashboard/components/revenue-chart.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+import { revenueData } from "../constants/revenue-data";
+
 export function RevenueChart() {
-  return <div>Revenue Chart</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Revenue Overview</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <div className="h-[350px]">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={revenueData}>
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+
+              <Line type="monotone" dataKey="revenue" strokeWidth={3} />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
